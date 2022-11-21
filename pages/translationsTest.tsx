e1a7f04b6cb0834c7a8374c8c00c0f4613f43051
Joseph Kim <josephkim828@gmail.com>
2022-11-21 17:11:55 +0900

    update translation error

--- pages/translationsTest.tsx
+++ pages/translationsTest.tsx
@@ -1,19 +1,15 @@
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Link from "next/link";
-import { getCurrUser } from "../src/services/users.api";
 
-export default function Translations({ user }) {
+export default function Translations() {
   const { t } = useTranslation("common");
 
   return (
     <div>
-      <p>{t("h1")}</p>
-      <p>{t("change-locale", { what: user[0].name ? user[0].name : "hi" })}</p>
-      <p>{t("to-second-page")}</p>
-      <p>{t("error-with-status")}</p>
-      <p>{t("error-without-status")}</p>
-      <p>{t("title")}</p>
+      <p>{t("fitness")}</p>
+      <p>{t("reservation")}</p>
+      <p>{t("membership")}</p>
       <div>
         <Link href="/translationsTest" locale="en">
           <button>영어</button>
@@ -27,13 +23,8 @@ export default function Translations({ user }) {
 }
 
 export async function getStaticProps({ locale }) {
-  console.log("locale of getStaticProps", locale);
-  const user = await getCurrUser(2);
-  console.log(user);
-
   return {
     props: {
-      user,
       ...(await serverSideTranslations(locale, ["common"])),
     },
   };
