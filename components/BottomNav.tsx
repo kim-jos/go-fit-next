f77bb28881d4c0b1a76d12c90f11137f934ef187
Joseph Kim <josephkim828@gmail.com>
2022-11-25 13:49:11 +0900

    updated navigation components

--- components/BottomNav.tsx
+++ components/BottomNav.tsx
@@ -1,38 +1,22 @@
-import AddIcon from "@mui/icons-material/Add";
-import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
-import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import ChatIcon from "@mui/icons-material/Chat";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
-import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
-import { Grid, styled, Typography } from "@mui/material";
+import PersonIcon from "@mui/icons-material/Person";
+import { Grid, Typography } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
-import Fab from "@mui/material/Fab";
 import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
+import Link from "next/link";
 import * as React from "react";
-
-const StyledFab = styled(Fab)({
-  //   position: "absolute",
-  //   zIndex: 1,
-  //   top: -30,
-  //   left: 0,
-  //   right: 0,
-  //   margin: "0 auto",
-  margin: 0,
-  top: "auto",
-  right: 30,
-  bottom: 100,
-  left: "auto",
-  position: "fixed",
-});
 
 export default function BottomAppBar() {
   return (
     <React.Fragment>
       <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
         <Toolbar>
-          <Grid container spacing={1}>
+          <Grid container>
             <Grid
-              xs={3}
+              container
+              xs={4}
               display="flex"
               justifyContent="center"
               alignItems="center"
@@ -40,12 +24,15 @@ export default function BottomAppBar() {
               item
             >
               <IconButton color="inherit" aria-label="open drawer">
-                <FitnessCenterIcon />
+                <Link href="/classes">
+                  <FitnessCenterIcon />
+                </Link>
               </IconButton>
               <Typography variant="subtitle2">Fitness</Typography>
             </Grid>
             <Grid
-              xs={3}
+              container
+              xs={4}
               display="flex"
               justifyContent="center"
               alignItems="center"
@@ -53,12 +40,15 @@ export default function BottomAppBar() {
               item
             >
               <IconButton color="inherit">
-                <CheckBoxIcon />
+                <Link href="/chat">
+                  <ChatIcon />
+                </Link>
               </IconButton>
-              <Typography variant="subtitle2">Reservations</Typography>
+              <Typography variant="subtitle2">Chat</Typography>
             </Grid>
             <Grid
-              xs={3}
+              container
+              xs={4}
               display="flex"
               justifyContent="center"
               alignItems="center"
@@ -66,29 +56,15 @@ export default function BottomAppBar() {
               item
             >
               <IconButton color="inherit">
-                <AddShoppingCartIcon />
+                <Link href="/profile">
+                  <PersonIcon />
+                </Link>
               </IconButton>
-              <Typography variant="subtitle2">Membership</Typography>
-            </Grid>
-            <Grid
-              xs={3}
-              display="flex"
-              justifyContent="center"
-              alignItems="center"
-              direction="column"
-              item
-            >
-              <IconButton color="inherit">
-                <MoreHorizIcon />
-              </IconButton>
-              <Typography variant="subtitle2">More</Typography>
+              <Typography variant="subtitle2">Profile</Typography>
             </Grid>
           </Grid>
         </Toolbar>
       </AppBar>
-      <StyledFab color="secondary" aria-label="add">
-        <AddIcon />
-      </StyledFab>
     </React.Fragment>
   );
 }
