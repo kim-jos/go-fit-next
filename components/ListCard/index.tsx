d2e5fb6330a40badebed11ebea58fa434f988bc1
kimyerim1935 <kimyerim1935@naver.com>
2022-11-25 18:49:52 +0900

    add: main page list components add (+style)

---
+++ components/ListCard/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './listCard.module.css';
+
+const ListCard = () => {
+    return (
+        <>
+            <span className={styles.listTitle}>Popular List</span>
+        </>
+    )
+}
+
+export default ListCard;
