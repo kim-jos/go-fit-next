1c818fb2db75102189c0d86317f09e58ba6e53be
Joseph Kim <josephkim828@gmail.com>
2022-11-26 00:22:35 +0900

    added colors

---
+++ styles/palette.ts
@@ -0,0 +1,33 @@
+// #481D52 primary color
+// #DD517E
+
+const palette = {
+  primary: {
+    main: "#481D52",
+    opacity02: "rgba(236, 95, 95, 1)",
+    light: "#3D393508",
+  },
+  secondary: {
+    main: "#DD517E",
+    sub: "rgba(254, 114, 114, 1)",
+  },
+  error: {
+    main: "#DE5D5D",
+  },
+  background: {
+    default: "#f3f3f3",
+  },
+  common: {
+    white: "#ffffff",
+    black: "#3D3935",
+  },
+  text: {
+    primary: "#333333",
+    secondary: "#333333",
+    label: "#333333b3",
+  },
+  border: {
+    main: "rgba(0, 0, 0, 0.2)",
+  },
+};
+export default palette;
