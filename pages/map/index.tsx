155e981d31eb0ed2ba5ad0c2fff3298b1f3bc3b3
Connor Smiley <60119998+ConnorSmiley@users.noreply.github.com>
2022-11-28 10:55:33 +0900

    mapped-added-v2

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,15 +1,17 @@
-import { Map, MapMarker } from "react-kakao-maps-sdk"
+import {Map, MapMarker} from "react-kakao-maps-sdk"
 
 function Home() {
     return (
-        <Map
-            center={{ lat: 33.5563, lng: 126.79581 }}
-            style={{ width: "100%", height: "360px" }}
-        >
-            <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-                <div style={{ color: "#000" }}>Hello World!</div>
-            </MapMarker>
-        </Map>
+        <>
+            <Map
+                center={{lat: 33.5563, lng: 126.79581}}
+                style={{width: "100%", height: "600px"}}
+            >
+                <MapMarker position={{lat: 33.55635, lng: 126.795841}}>
+                    <div style={{color: "#000"}}>Location</div>
+                </MapMarker>
+            </Map>
+        </>
     )
 }
 
