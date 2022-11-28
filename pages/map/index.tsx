441b2a7f7aebf218c13086c08fc8efe36c2eb15e
Joseph Kim <josephkim828@gmail.com>
2022-11-28 21:10:32 +0900

    added maps feature

--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,18 +1,18 @@
-import {Map, MapMarker} from "react-kakao-maps-sdk"
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 function Home() {
-    return (
-        <>
-            <Map
-                center={{lat: 33.5563, lng: 126.79581}}
-                style={{width: "100%", height: "600px"}}
-            >
-                <MapMarker position={{lat: 33.55635, lng: 126.795841}}>
-                    <div style={{color: "#000"}}>Location</div>
-                </MapMarker>
-            </Map>
-        </>
-    )
+  return (
+    <>
+      <Map
+        center={{ lat: 33.5563, lng: 126.79581 }}
+        style={{ width: "100%", height: "600px" }}
+      >
+        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+          <div style={{ color: "#000" }}>Location</div>
+        </MapMarker>
+      </Map>
+    </>
+  );
 }
 
-export default Home
+export default Home;
