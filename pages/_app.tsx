13ce47770470c4a7bf08ca7a82a118619efdc539
Joseph Kim <josephkim828@gmail.com>
2022-12-04 02:35:06 +0900

    added push notifications

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,19 +6,35 @@ import { SessionProvider } from "next-auth/react";
 import { appWithTranslation } from "next-i18next";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
+import { useEffect } from "react";
+import OneSignal from "react-onesignal";
 import BottomNav from "../components/BottomNav";
 import TopAppBar from "../components/TopAppBar";
-import { kakaoKey } from "../src/utils/keys";
+import { oneSignalKey } from "../src/utils/keys";
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  useEffect(() => {
+    OneSignal.init({
+      appId: oneSignalKey,
+      allowLocalhostAsSecureOrigin: true,
+    }).then(() => {
+      OneSignal.showSlidedownPrompt();
+    });
+  }, []);
+
   return (
     <SessionProvider session={session}>
+      {/* <Script
+        src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoKey}&libraries=services,clusterer&autoload=false`}
+        strategy="beforeInteractive"
+      /> */}
+      <Script
+        src="https://cdn.onesignal.com/sdks/OneSignalSDK.js"
+        strategy="afterInteractive"
+      />
       <Head>
         <link rel="shortcut icon" href="/GoFit_Logo.svg" />
-        <script
-          type="text/javascript"
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoKey}&libraries=services`}
-        />
       </Head>
       <TopAppBar />
       <Component {...pageProps} />
--- src/utils/keys.ts
+++ src/utils/keys.ts
@@ -1,1 +1,2 @@
 export const kakaoKey = process.env.NEXT_PUBLIC_KAKAO_JS_KEY!;
+export const oneSignalKey = process.env.NEXT_PUBLIC_ONE_SIGNAL_APP_ID!;
