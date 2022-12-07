c0a1b2dfb32e824e7c5634411d2e143a18f77593
ske-kr <54828276+ske-kr@users.noreply.github.com>
2022-12-07 14:41:23 +0900

    feat:tailwind added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import 'tailwindcss/tailwind.css';
 import { User } from "@sendbird/chat";
 import { SessionProvider } from "next-auth/react";
 import { appWithTranslation } from "next-i18next";
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,6 @@ import { SessionUser } from "../src/utils/session.type";
 
 export default function Home() {
   const [isActive, setIsActive] = useState(false);
-  const [email, setEmail] = useState<string>();
-  const [password, setPassword] = useState<string>();
   const { data } = useSession();
   const user: SessionUser = data?.user;
 
@@ -16,24 +14,30 @@ export default function Home() {
   };
 
   return (
-    <>
-      <div>홈페이지</div>
-      {data ? (
-        <div>
-          <div>{user?.name}</div>
-          <div>{user?.email}</div>
-          {`${user.id}`}
-          {`${user.accessToken}`}
-          <div></div>
-          {`${user?.user_id}`}
-        </div>
-      ) : (
-        <div>not logged in</div>
-      )}
-
-      <Button variant="contained" onClick={() => setIsActive(true)}>
-        로그인
-      </Button>
+    <div className={"w-full flex flex-col justify-center"}>
+      <div className={"text-xl mt-4 w-full text-center"}>홈페이지</div>
+      <div className={"text-center"}>
+        {data ? (
+          <div>
+            <div>{user?.name}</div>
+            <div>{user?.email}</div>
+            {`${user.id}`}
+            {`${user.accessToken}`}
+            <div></div>
+            {`${user?.user_id}`}
+          </div>
+        ) : (
+          <div>not logged in</div>
+        )}
+      </div>
+      <div className={"flex w-full justify-center"}>
+        <button
+          className={"bg-blue-500 rounded-lg px-3 py-1.5 text-white shadow-lg"}
+          onClick={() => setIsActive(true)}
+        >
+          로그인
+        </button>
+      </div>
       <ModalBase active={isActive} closeEvent={onClickModalOff}>
         <Stack>
           {/* closeEvent={onClickModalOff} title="Welcome Back" */}
@@ -41,7 +45,7 @@ export default function Home() {
             <Button
               onClick={() =>
                 signIn("CredentialsProvider", {
-                  callbackUrl: "http://localhost:3000/classes",
+                  callbackUrl: "/classes",
                 })
               }
               color="primary"
@@ -54,6 +58,6 @@ export default function Home() {
           </div>
         </Stack>
       </ModalBase>
-    </>
+    </div>
   );
 }
